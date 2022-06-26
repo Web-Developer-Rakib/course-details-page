b413e4fce03a1537250ea1b65da3887f5d33c6bc
WebDeveloperApolo <rakibodesk7@gmail.com>
2022-06-26 22:07:08 +0600

    design done

--- src/Components/Course.js
+++ src/Components/Course.js
@@ -1,4 +1,4 @@
-import { faHeart } from "@fortawesome/free-regular-svg-icons";
+import { faHeart, faQuestionCircle } from "@fortawesome/free-regular-svg-icons";
 import {
   faArrowRight,
   faShare,
@@ -15,20 +15,32 @@ const Course = ({ pageInfo }) => {
     teachersReviews,
     classReviews,
     completedLearners,
+    teachersImg,
     courseImg,
   } = pageInfo;
   return (
     <div className=" pl-1 pr-1 lg:pl-5 lg:pr-30 pt-5">
       <h1 className="text-5xl font-bold">{title}</h1>
-      <h3 className="text-2xl font-semibold text-gray-500 mt-1">{subTitle}</h3>
+      <h3 className="text-2xl font-semibold text-gray-500 mt-2">
+        {subTitle}
+        <FontAwesomeIcon
+          className="ml-2"
+          icon={faQuestionCircle}
+        ></FontAwesomeIcon>
+      </h3>
       <div className="mt-20 flex justify-around flex-wrap-reverse lg:flex-nowrap">
         <div>
           <p className="lg:mr-60 w-full lg:w-[500px] font-semibold mt-5 lg:mt-0">
             {desc}
           </p>
-          <div>
-            {" "}
-            <h4 className="text-xl text-primary font-semibold mt-5">
+          <div className="flex items-center mt-5">
+            <img
+              src={teachersImg}
+              className="rounded-full mr-2"
+              alt=""
+              width={40}
+            />
+            <h4 className="text-xl text-primary font-semibold">
               {teachersName}
             </h4>
           </div>
